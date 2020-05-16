454486cfd2ec4699ec0a205d61e36fd1227d52e8
Onur Korkmaz <onurkorkmaz@Onurs-MacBook-Pro-2.local>
2020-05-16 19:29:45 +0200

    adds a python script that prints a message greeting Github

---
+++ python_code.py
@@ -0,0 +1,53 @@
+Last login: Sat May 16 18:33:53 on ttys000
+
+The default interactive shell is now zsh.
+To update your account to use zsh, please run `chsh -s /bin/zsh`.
+For more details, please visit https://support.apple.com/kb/HT208050.
+Onurs-MacBook-Pro-2:~ onurkorkmaz$ cd ~/Desktop/projects/Capstone
+Onurs-MacBook-Pro-2:Capstone onurkorkmaz$ print("Hello Github!")
+-bash: !": event not found
+Onurs-MacBook-Pro-2:Capstone onurkorkmaz$ ls
+README.md
+Onurs-MacBook-Pro-2:Capstone onurkorkmaz$ cd ~/Desktop/projects/Capstone
+Onurs-MacBook-Pro-2:Capstone onurkorkmaz$ print("Hello Github!")
+-bash: !": event not found
+Onurs-MacBook-Pro-2:Capstone onurkorkmaz$ emacs python_code.py
+-bash: emacs: command not found
+Onurs-MacBook-Pro-2:Capstone onurkorkmaz$ open -a Emacs
+Unable to find application named 'Emacs'
+Onurs-MacBook-Pro-2:Capstone onurkorkmaz$ $ brew cask install emacs
+-bash: $: command not found
+Onurs-MacBook-Pro-2:Capstone onurkorkmaz$ brew cash install emacs
+-bash: brew: command not found
+Onurs-MacBook-Pro-2:Capstone onurkorkmaz$ sudo port install emacs-app
+Password:
+sudo: port: command not found
+Onurs-MacBook-Pro-2:Capstone onurkorkmaz$ open -a emacs
+Onurs-MacBook-Pro-2:Capstone onurkorkmaz$ emacs python_code.py
+-bash: emacs: command not found
+Onurs-MacBook-Pro-2:Capstone onurkorkmaz$ vim python_code.py
+
+print('Hello Github!')
+
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+-- INSERT --
